48627b2c0b3530c4ddc35541305ce7e00aad6a90
iamnghiaxhoang <nghiaxhoang1@gmail.com>
2026-09-03 01:11:59 +0700

    feat: implement nn model and utils function

---
+++ model.py
@@ -0,0 +1,77 @@
+import numpy as np
+numsOfNeuron = 16
+numsOfOutput = 10
+imgSize = 784
+
+def calRelu(Z):
+    return np.max(0, Z);
+
+def calReluDerivative(Z):
+    return Z > 0;
+
+def calSoftmax(Z):
+    # use axis=0 to compute sample seperately (mini-batches)
+    expZ = np.exp(Z - np.max(Z, axis=0));
+    return expZ / np.sum(expZ, axis=0);
+
+def encodeOneHot(Y):
+    temp = np.zeros(Y.size, Y.max() + 1);
+    temp[np.arange(Y.size), Y] = 1;
+    return temp.T;
+
+def initializeParameters():
+    # will optimize with He Initialization later
+    W1 = np.random.rand(numsOfNeuron, imgSize) - 0.5;
+    b1 = np.random.rand(numsOfNeuron, 1) - 0.5;
+    W2 = np.random.rand(numsOfNeuron, numsOfNeuron) - 0.5;
+    b2 = np.random.rand(numsOfNeuron, 1) - 0.5;
+    W3 = np.random.rand(numsOfNeuron, numsOfNeuron) - 0.5;
+    b3 = np.random.rand(numsOfOutput, 1) - 0.5;
+    return W1, b1, W2, b2, W3, b3;
+
+def propagateForward(X, W1, b1, W2, b2, W3, b3):
+    Z1 = W1.dot(X) + b1;
+    A1 = calRelu(Z1); 
+    Z2 = W2.dot(A1) + b2;
+    A2 = calRelu(Z2);
+    Z3 = W3.dot(A2) + b3;
+    A3 = calSoftmax(Z3);
+    return Z1, A1, Z2, A2, Z3, A3 
+
+def propagateBackward(Z1, A1, Z2, A2, Z3, A3, W1, W2, W3, X, Y):
+    m = Y.size
+    dZ3 = A3 - encodeOneHot(Y);
+    dW3 = 1/m * dZ3.dot(A2.T);
+    db3 = 1/m * np.sum(dZ3, axis=1, keepdims=True);
+
+    dZ2 = W3.T.dot(dZ3) * calReluDerivative(Z2);
+    dW2 = 1/m * dZ2.dot(A1.T);
+    db2 = 1/m * np.sum(dZ2, axis=1, keepdims=True);
+
+    dZ1 = W2.T.dot(dZ2) * calReluDerivative(Z1);
+    dW1 = 1/m * dZ1.dot(X.T);
+    db1 = 1/m * np.sum(dZ1, axis=1, keepdims=True);
+    return dW1, db1, dW2, db2;
+
+def updateParameters(W1, b1, W2, b2, W3, b3, dW1, db1, dW2, db2, dW3, db3, alpha):
+    W1 = W1 - alpha * dW1;
+    b1 = b1 - alpha * db1;
+    W2 = W2 - alpha * dW2;
+    b2 = b2 - alpha * db2;
+    W3 = W3 - alpha * dW3;
+    b3 = b3 - alpha * db3;
+    return W1, b1, W2, b2, W3, b3;
+
+def getPredictions(A2):
+    return np.argmax(A2, 0);
+
+def calculateAccuracy(predictions, Y):
+    return np.sum(predictions == Y) / Y.size;
+
+def performGradientDescent(X, Y, alpha, iterations):
+    W1, b1, W2, b2 = initializeParameters();
+    for i in range(iterations):
+        Z1, A1, Z2, A2 = propagateForward(X, W1, b1, W2, b2);
+        dW1, db1, dW2, db2 = propagateBackward(Z1, A1, Z2, A2, W1, W2, X, Y);
+        W1, b1, W2, b2 = updateParameters(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha);
+    return W1, b1, W2, b2
